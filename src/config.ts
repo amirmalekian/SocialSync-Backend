7aed02ddef24628a0bbd360381b56b340b0912ce
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2023-02-28 17:28:34 +0330

    chore: :wrench: add config files

---
+++ src/config.ts
@@ -0,0 +1,18 @@
+import { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+export default (app: Application, express: any) => {
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+  app.use(
+    cors({
+      origin: "http://localhost:5174",
+    })
+  );
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+};
