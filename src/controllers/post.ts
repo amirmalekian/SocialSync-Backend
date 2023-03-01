48a66a921e834974cd83778feb60e8f1fa9465db
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2023-03-01 19:07:02 +0330

    feat(post): :sparkles: add addPost API to create the post

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
 import { db } from "../db";
 import config from "config";
+import moment from "moment";
 
 export const getPosts = (req: Request, res: Response) => {
   const token = req.cookies.accessToken;
@@ -17,6 +18,29 @@ export const getPosts = (req: Request, res: Response) => {
     db.query(query, [userInfo.id, userInfo.id], (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json(data);
+    });
+  });
+};
+
+export const addPost = (req: Request, res: Response) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in");
+
+  jwt.verify(token, config.get("jwt_key"), (err: any, userInfo: any) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const query =
+      "INSERT INTO posts (`desc`, `img`, `createdAt`, `userId`) VALUES (?)";
+
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+    ];
+    db.query(query, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post has been created");
     });
   });
 };
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import {getPosts} from "../controllers/post";
+import { getPosts, addPost } from "../controllers/post";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.post("/", addPost);
 
 export default router;
