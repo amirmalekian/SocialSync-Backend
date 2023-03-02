7a1bb9b0c5790cd66c075eb6f3619ad18a2bea62
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2023-03-02 10:28:40 +0330

    feat(config): :wrench: add multer config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { Application } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 export default (app: Application, express: any) => {
   app.use((req, res, next) => {
@@ -15,4 +16,20 @@ export default (app: Application, express: any) => {
   app.use(cookieParser());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "../client/public/upload");
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname);
+    },
+  });
+
+  const upload = multer({ storage: storage });
+
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.status(200).json(file?.filename);
+  });
 };
