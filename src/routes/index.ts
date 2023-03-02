4600c2f24d6977a4c7e4561c8274ca2e643288de
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2023-03-02 17:14:30 +0330

    feat(like): :sparkles: add like and dislike for posts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,13 @@ import express from "express";
 import authRoutes from "./auth";
 import postRoutes from "./posts";
 import commentRoutes from "./comments";
+import likeRoutes from "./likes";
 
 const router = express.Router();
+
 router.use("/auth", authRoutes);
 router.use("/posts", postRoutes);
 router.use("/comments", commentRoutes);
+router.use("/likes", likeRoutes);
 
 export default router;
