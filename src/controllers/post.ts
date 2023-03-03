6caf75a88faf26b262d7405d13cc07241d3c3a82
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2023-03-03 09:03:14 +0330

    refactor(post): :zap: separate the fetching of all users' posts from the logged-in user's posts on her profile page

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -5,17 +5,23 @@ import config from "config";
 import moment from "moment";
 
 export const getPosts = (req: Request, res: Response) => {
+  const userId = req.query.userId;
   const token = req.cookies.accessToken;
   if (!token) return res.status(401).json("Not logged in");
 
   jwt.verify(token, config.get("jwt_key"), (err: any, userInfo: any) => {
     if (err) return res.status(403).json("Token is not valid!");
 
-    const query = `SELECT p.*, u.id AS userId, name, profileImg FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+    const query = userId
+      ? `SELECT p.*, u.id AS userId, name, profileImg FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ?`
+      : `SELECT p.*, u.id AS userId, name, profileImg FROM posts AS p JOIN users AS u ON (u.id = p.userId)
     LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?
     ORDER BY p.createdAt DESC
     `;
-    db.query(query, [userInfo.id, userInfo.id], (err, data) => {
+
+    const values = userId ? [userId] : [userInfo.id, userInfo.id];
+
+    db.query(query, values, (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json(data);
     });
