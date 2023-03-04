9bf5289215bc5e608cfd0ba3bda9cff32c613643
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2023-03-04 07:55:38 +0330

    feat(post): :sparkles: add deletePost router

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -12,14 +12,16 @@ export const getPosts = (req: Request, res: Response) => {
   jwt.verify(token, config.get("jwt_key"), (err: any, userInfo: any) => {
     if (err) return res.status(403).json("Token is not valid!");
 
-    const query = userId
-      ? `SELECT p.*, u.id AS userId, name, profileImg FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ?`
-      : `SELECT p.*, u.id AS userId, name, profileImg FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+    const query =
+      userId !== "undefined"
+        ? `SELECT p.*, u.id AS userId, name, profileImg FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
+        : `SELECT p.*, u.id AS userId, name, profileImg FROM posts AS p JOIN users AS u ON (u.id = p.userId)
     LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?
     ORDER BY p.createdAt DESC
     `;
 
-    const values = userId ? [userId] : [userInfo.id, userInfo.id];
+    const values =
+      userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
 
     db.query(query, values, (err, data) => {
       if (err) return res.status(500).json(err);
@@ -47,6 +49,24 @@ export const addPost = (req: Request, res: Response) => {
     db.query(query, [values], (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json("Post has been created");
+    });
+  });
+};
+
+export const deletePost = (req: Request, res: Response) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in");
+
+  jwt.verify(token, config.get("jwt_key"), (err: any, userInfo: any) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const query = "DELETE FROM posts WHERE `id` = ? AND `userId` = ?";
+
+    db.query(query, [req.params.id, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if (data.affectedRows > 0)
+        return res.status(200).json("Post has been deleted.");
+      return res.status(403).json("You can delete only your post");
     });
   });
 };
