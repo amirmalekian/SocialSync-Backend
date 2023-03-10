1f17a9dc46204f312464350e7891da57c4dd8f78
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2023-03-10 11:14:36 +0330

    feat(story): :sparkles: add story route with contorller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import commentRoutes from "./comments";
 import likeRoutes from "./likes";
 import userRoutes from "./users";
 import relationshipRoutes from "./relationships";
+import storyRoutes from "./stories";
 
 const router = express.Router();
 
@@ -14,5 +15,6 @@ router.use("/comments", commentRoutes);
 router.use("/likes", likeRoutes);
 router.use("/users", userRoutes);
 router.use("/relationships", relationshipRoutes);
+router.use("/stories", storyRoutes);
 
 export default router;
